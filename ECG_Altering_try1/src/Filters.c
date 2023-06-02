6ac250b09b64141c2ea2c3b6e9af08f4683e48e1
Chris <Chris@LAPTOP-CQNEDNM8>
2023-06-02 12:17:56 +0200

    added filter source and header

---
+++ ECG_Altering_try1/src/Filters.c
@@ -0,0 +1,14 @@
+/*
+ * Filters.c
+ *
+ *  Created on: 2 jun. 2023
+ *      Author: Chris
+ */
+
+float outputArray[3];
+
+float SecondOrderFilter( float dataIn[3], float p_fCoef[2])
+{
+
+}
+outputlowpassA[n]= -2*outputlowpassA[n-1] - outputlowpassA[n-2] + arrayECG[n] - 1.79396184525177*arrayECG[n-1] + 0.886283112007014*arrayECG[n-2];
---
+++ ECG_Altering_try1/src/Filters.h
@@ -0,0 +1,16 @@
+/*
+ * Filters.h
+ *
+ *  Created on: 2 jun. 2023
+ *      Author: Chris
+ */
+
+#ifndef SRC_FILTERS_H_
+#define SRC_FILTERS_H_
+
+
+
+#endif /* SRC_FILTERS_H_ */
+
+
+float SecondOrderFilter( float p_dataArray[3], float filterCoefficients[5]);
